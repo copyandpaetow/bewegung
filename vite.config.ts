07aa7b70d658b9ad3544c2e8cc6c1484bdf02106
LucasPaetow <lucas.paetow@gmail.com>
2022-04-11 00:00:37 +0200

    feat: automate deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import { defineConfig } from "vite";
 module.exports = defineConfig({
 	base: "/bewegung/",
 	build: {
-		lib: {
-			entry: path.resolve(__dirname, "src/lib/bewegung.ts"),
-			name: "bewegung",
-			fileName: (format) => `bewegung.${format}.js`,
-		},
 		rollupOptions: {},
 	},
 });
