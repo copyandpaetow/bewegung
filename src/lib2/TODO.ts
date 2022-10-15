c93c2147c7595e80b200ac860e51d5b2ed464379
LucasPaetow <lucas.paetow@gmail.com>
2022-10-15 12:51:01 +0200

    task: update todos

--- src/lib2/TODO.ts
+++ src/lib2/TODO.ts
@@ -1,16 +1,4 @@
 /*
-
-TODOS:
-- how to include RAF in the scheduler for DOM work? currently it will delay everything or increase the calculation duration by a lot
-? maybe the current implementation still works fine without raf for the dom work
-
-- it would be nice to separate the initial state into their own blocks, but its unclear if it is feasible 
-
-
-
-*/
-
-/*
 tasks:
 
 - normalize inputs
@@ -66,4 +54,53 @@ case MO2: primary element gets added / deleted without closing the entry => add 
 case MO3: primary element gets added / deleted with closing the entry => remove the empty array and every other entry with that index from the other states and recalc second elements
 
 
+*/
+
+/*
+- dom element as a key is very nice to work with (weakmaps or maps) 
+	=> because it has a reference to related elements to it (parent, children, next/prev Sibling)
+	=> this could be emulated though with a key-object. We would need to use a weakRef or a translation for the elements to keys
+	=> with this we could re-assign in case of filtering but it adds a layer of complexity. 
+	   It would also be tricky to search where the element is used so we could replace the parent
+	**we will use dom elements**
+
+- storing stuff in lists and then iterating them without changing to something else is good for performance (StructureOfArrays > ArrayOfStructure)
+- having many store locations is more performant than a key-value pair, where the value is a huge and everchanging object
+- index as a sole connection is error prone, especially if for filtering reasons, the dom element is still there, just not within the library
+
+- main- and secondary elements should have the same data structure because they will often be used the same way 
+- should all elements be in all stores? Or should the secondary elements be in some? 
+
+- since the elements can be in multiple chunks, the stores should always return an array
+- secondary elements dependece on primary ones are needed for the options (timeline) and for the selectors (MO)
+
+
+*/
+
+const mainElements = new Set<HTMLElement>();
+const mainElements2: HTMLElement[] = [];
+
+const keyframes = new WeakMap<HTMLElement, CSSStyleDeclaration[][]>();
+const callbacks = new WeakMap<HTMLElement, VoidFunction[][]>();
+const options = new WeakMap<HTMLElement, EffectTiming[]>();
+const rootElement = new WeakMap<HTMLElement, HTMLElement>();
+
+const secondaryElements = new Set<HTMLElement>();
+const secondaryElements2: HTMLElement[] = [];
+
+/*
+case postprocess => remove sec from secondaryElements + recalculate readouts
+case mo1 => remove (see above) / add to secondaryElements, get mainElements which would affect the new element, get their options + rootElement and add this combined
+for the secondary element
+case mo2/3: delete main element => recalc runtime, if it is different update keyframes and callbacks, start from secondaryElement calculation
+case mo4: add main element => create a selector ElementMap. Get the main elements for each matching selector and copy their values, get their secondary elements and add them,
+recalculate
+*/
+
+/*
+TODOS
+
+- change structure again to match the above vision, but keep the scheduling and the task layout
+- try to use as few "global"weakmap stores as possible
+- think about filtering some more. Currently, they are just Arrays of {..., offset}. They could also be a SoA or a map with offsets as keys
 */
--- src/lib2/read/calculate-keyframes.ts
+++ src/lib2/read/calculate-keyframes.ts
@@ -18,6 +18,8 @@ const calcualtionsFromReadouts = (
 	isTextNode: boolean
 ): DimensionalDifferences[] => {
 	console.log(readouts, parentReadouts);
+
+	//TODO: maybe here the filtering could take place and only locally and for the child so the entries are not affected but stuff will not get calculated multiple times
 	return readouts.map((readout, index, array) => {
 		const child: DifferenceArray = [readout, array.at(-1)!];
 		const parent: DifferenceArray = [parentReadouts[index], parentReadouts.at(-1)!];
