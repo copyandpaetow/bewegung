7d31d2d66313cad6a41281a221b99883ebda36a2
LucasPaetow <lucas.paetow@gmail.com>
2022-10-28 21:59:35 +0200

    fix: correct animations in grid layouts

--- src/lib2/worker-thread/animate-images.ts
+++ src/lib2/worker-thread/animate-images.ts
@@ -10,16 +10,10 @@ import { calculateEasingMap } from "./calculate-easings";
 import { highestNumber } from "./context";
 
 export const save = (value: number, alternative: number): number => {
-	return value === Infinity || value === -Infinity || isNaN(value)
-		? alternative
-		: value;
+	return value === Infinity || value === -Infinity || isNaN(value) ? alternative : value;
 };
 
-const calculateBorderRadius = (
-	borderRadius: string,
-	height: number,
-	width: number
-): string => {
+const calculateBorderRadius = (borderRadius: string, height: number, width: number): string => {
 	const parsedRadius = parseFloat(borderRadius);
 
 	if (isNaN(parsedRadius)) {
@@ -34,12 +28,8 @@ interface MaximumDimensions {
 	width: number;
 	height: number;
 }
-const getMaximumDimensions = (
-	styleMap: calculatedElementProperties[]
-): MaximumDimensions => {
-	const maxHeight = highestNumber(
-		styleMap.map((prop) => prop.dimensions.height)
-	);
+const getMaximumDimensions = (styleMap: calculatedElementProperties[]): MaximumDimensions => {
+	const maxHeight = highestNumber(styleMap.map((prop) => prop.dimensions.height));
 
 	const maxWidth = highestNumber(styleMap.map((prop) => prop.dimensions.width));
 
@@ -64,6 +54,7 @@ const getWrapperStyle = (
 		width: `${maxValues.width}px`,
 		pointerEvents: "none",
 		overflow: "hidden",
+		gridArea: "1/1/2/2",
 	} as Partial<CSSStyleDeclaration>;
 };
 
@@ -77,12 +68,9 @@ const getWrapperKeyframes = (
 		const horizontalInset = (maxWidth - entry.dimensions.width) / 2;
 		const verticalInset = (maxHeight - entry.dimensions.height) / 2;
 
-		const deltaTop =
-			entry.dimensions.top -
-			(array.at(-1)?.dimensions.top || entry.dimensions.top);
+		const deltaTop = entry.dimensions.top - (array.at(-1)?.dimensions.top || entry.dimensions.top);
 		const deltaLeft =
-			entry.dimensions.left -
-			(array.at(-1)?.dimensions.left || entry.dimensions.left);
+			entry.dimensions.left - (array.at(-1)?.dimensions.left || entry.dimensions.left);
 		const translateX = -1 * horizontalInset + deltaLeft;
 		const translateY = -1 * verticalInset + deltaTop;
 
@@ -135,27 +123,20 @@ const calculateImageKeyframes = (
 						return (parseFloat(value) - 100) / 100;
 					}
 					return (
-						parseFloat(value) /
-						(index === 0 ? entry.dimensions.width : entry.dimensions.height)
+						parseFloat(value) / (index === 0 ? entry.dimensions.width : entry.dimensions.height)
 					);
 				});
 
-			translateX =
-				save((maxWidth * scaleWidth - entry.dimensions.width) / 2, 0) *
-				xAchis *
-				-1;
-			translateY =
-				save((maxHeight * scaleHeight - entry.dimensions.height) / 2, 0) *
-				yAchis *
-				-1;
+			translateX = save((maxWidth * scaleWidth - entry.dimensions.width) / 2, 0) * xAchis * -1;
+			translateY = save((maxHeight * scaleHeight - entry.dimensions.height) / 2, 0) * yAchis * -1;
 		}
 
 		return {
 			offset: entry.offset,
-			transform: `translate(${translateX}px, ${translateY}px) scale(${save(
-				scaleWidth,
+			transform: `translate(${translateX}px, ${translateY}px) scale(${save(scaleWidth, 1)}, ${save(
+				scaleHeight,
 				1
-			)}, ${save(scaleHeight, 1)})`,
+			)})`,
 			easing: easingTable[entry.offset] ?? "ease",
 		};
 	});
@@ -173,10 +154,7 @@ export const getImageAnimations = (
 		image: PreAnimation;
 	}
 > => {
-	const imageAnimationMap = new Map<
-		string,
-		{ wrapper: PreAnimation; image: PreAnimation }
-	>();
+	const imageAnimationMap = new Map<string, { wrapper: PreAnimation; image: PreAnimation }>();
 
 	elementState.forEach((elementKey, idString) => {
 		if (elementKey.tagName !== "IMG") {
@@ -200,17 +178,9 @@ export const getImageAnimations = (
 
 		const wrapperStyle = getWrapperStyle(styleMap, rootStyleMap, maxDimensions);
 
-		const wrapperKeyframes = getWrapperKeyframes(
-			styleMap,
-			maxDimensions,
-			easings
-		);
+		const wrapperKeyframes = getWrapperKeyframes(styleMap, maxDimensions, easings);
 
-		const imageKeyframes = calculateImageKeyframes(
-			styleMap,
-			maxDimensions,
-			easings
-		);
+		const imageKeyframes = calculateImageKeyframes(styleMap, maxDimensions, easings);
 		const wrapperAnimation: PreAnimation = {
 			keyframes: wrapperKeyframes,
 			options: context.totalRuntime,
