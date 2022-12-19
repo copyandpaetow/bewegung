71f2f0dfc4a0b69f2452ffe151212f861bf60ccd
LucasPaetow <lucas.paetow@gmail.com>
2022-12-19 15:58:09 +0100

    fix: delay not working

--- src/lib2/animation.ts
+++ src/lib2/animation.ts
@@ -4,15 +4,12 @@ import { initState } from "./normalize/state";
 import { getAnimationInformation, readWriteDomChanges } from "./read/dom";
 import { BewegungProps } from "./types";
 
-//TODO: unify sendQuere und Listener Names in an enum
-//TODO: create an enum for the elementEntry types
+//TODO: unify sendQuere und Listener Names in a type / an enum
+//TODO: create a type for the elementEntry types
 //TODO: rethink the offset structure for the style entries. Finding entries with certain offsets is tedious
 //TODO: stop empty overrides from being send
-
-//! delay and enddelay will make the offsets smaller like without it, it goes from 0-1 but with them it could be 0.2 - 0.8
-// TODO: at some point we need to "fill" the first/last keyframe to have the same as the actual first/last keyframe
-// => first keyframe is 0.2, so it should be cloned with the offset 0
-// ! but only if this comes from delay/start
+//TODO: different root selector create a small jump in the beginning
+//TODO: display: none is not consistently working
 
 export const getAnimations = async (...props: BewegungProps) => {
 	const state = initState(...props);
